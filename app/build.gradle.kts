0511ef8e9f15bcf82b65b538185484e9d93cee11
amitseervi <amitchaudhary.adc@gmail.com>
2023-10-12 19:55:59 +0530

    Initializing Project

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+import java.util.Properties
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "com.amit.foodstory"
+    compileSdk = 34
+
+    signingConfigs {
+        val secretProperties = Properties()
+        secretProperties.load(File(rootDir,"secrets/keystore_credentials.properties").bufferedReader())
+        create("release"){
+            this.storeFile = File(rootDir,"secrets/keystore.jks")
+            this.enableV4Signing=true
+            this.keyAlias=secretProperties.getProperty("release.open.key")
+            this.keyPassword=secretProperties.getProperty("release.open.password")
+            this.storePassword=secretProperties.getProperty("release.open.keystore_pass")
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.amit.foodstory"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            this.signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+}
